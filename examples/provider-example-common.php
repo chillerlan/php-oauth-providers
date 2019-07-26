dba9609d23c19cdd6caf95e84b2d467f69e5f531
codemasher <smiley@chillerlan.net>
2019-07-26 22:59:40 +0200

    :sparkles:

---
+++ examples/provider-example-common.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * @filesource   provider-example-common.php
+ * @created      26.07.2019
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2019 smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\OAuthExamples;
+
+use chillerlan\OAuth\Providers\Spotify\Spotify;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$CFGDIR = __DIR__.'/../config';
+
+/** @var \chillerlan\Settings\SettingsContainerInterface $options */
+$options = null;
+
+require_once __DIR__.'/../vendor/chillerlan/php-oauth-core/examples/oauth-example-common.php';
+
+$providers = [
+];
