6e8a783dc24f2a63b105a1d19347c4f184295347
codemasher <smiley@chillerlan.net>
2021-03-22 22:08:55 +0100

    :shower: Twitch: update scopes

--- src/Twitch/Twitch.php
+++ src/Twitch/Twitch.php
@@ -14,10 +14,9 @@
 namespace chillerlan\OAuth\Providers\Twitch;
 
 use chillerlan\OAuth\Core\{AccessToken, ClientCredentials, CSRFToken, OAuth2Provider, TokenRefresh};
-
 use Psr\Http\Message\RequestInterface;
-use function http_build_query, implode;
 
+use function http_build_query, implode;
 use const PHP_QUERY_RFC1738;
 
 /**
@@ -26,41 +25,42 @@ use const PHP_QUERY_RFC1738;
  */
 class Twitch extends OAuth2Provider implements ClientCredentials, CSRFToken, TokenRefresh{
 
-	public const SCOPE_CHANNEL_CHECK_SUBSCRIPTION = 'channel_check_subscription';
-	public const SCOPE_CHANNEL_COMMERCIAL         = 'channel_commercial';
-	public const SCOPE_CHANNEL_EDITOR             = 'channel_editor';
-	public const SCOPE_CHANNEL_FEED_EDIT          = 'channel_feed_edit';
-	public const SCOPE_CHANNEL_FEED_READ          = 'channel_feed_read';
-	public const SCOPE_CHANNEL_CHANNEL_READ       = 'channel_read';
-	public const SCOPE_CHANNEL_CHANNEL_STREAM     = 'channel_stream';
-	public const SCOPE_CHANNEL_SUBSCRIPTIONS      = 'channel_subscriptions';
-	public const SCOPE_CHAT_LOGIN                 = 'chat_login';
-	public const SCOPE_COLLECTIONS_EDIT           = 'collections_edit';
-	public const SCOPE_COMMUNITIES_EDIT           = 'communities_edit';
-	public const SCOPE_COMMUNITIES_MODERATE       = 'communities_moderate';
-	public const SCOPE_OPENID                     = 'openid';
-	public const SCOPE_USER_BLOCKS_EDIT           = 'user_blocks_edit';
-	public const SCOPE_USER_BLOCKS_READ           = 'user_blocks_read';
-	public const SCOPE_USER_FOLLOWS_EDIT          = 'user_follows_edit';
-	public const SCOPE_USER_READ                  = 'user_read';
-	public const SCOPE_USER_SUBSCRIPTIONS         = 'user_subscriptions';
-	public const SCOPE_VIEWING_ACTIVITY_READ      = 'viewing_activity_read';
+	public const SCOPE_ANALYTICS_READ_EXTENSIONS  = 'analytics:read:extensions';
+	public const SCOPE_ANALYTICS_READ_GAMES       = 'analytics:read:games';
+	public const SCOPE_BITS_READ                  = 'bits:read';
+	public const SCOPE_CHANNEL_EDIT_COMMERCIAL    = 'channel:edit:commercial';
+	public const SCOPE_CHANNEL_MANAGE_BROADCAST   = 'channel:manage:broadcast';
+	public const SCOPE_CHANNEL_MANAGE_EXTENSIONS  = 'channel:manage:extensions';
+	public const SCOPE_CHANNEL_MANAGE_REDEMPTIONS = 'channel:manage:redemptions';
+	public const SCOPE_CHANNEL_MANAGE_VIDEOS      = 'channel:manage:videos';
+	public const SCOPE_CHANNEL_READ_EDITORS       = 'channel:read:editors';
+	public const SCOPE_CHANNEL_READ_HYPE_TRAIN    = 'channel:read:hype_train';
+	public const SCOPE_CHANNEL_READ_REDEMPTIONS   = 'channel:read:redemptions';
+	public const SCOPE_CHANNEL_READ_STREAM_KEY    = 'channel:read:stream_key';
+	public const SCOPE_CHANNEL_READ_SUBSCRIPTIONS = 'channel:read:subscriptions';
+	public const SCOPE_CLIPS_EDIT                 = 'clips:edit';
+	public const SCOPE_MODERATION_READ            = 'moderation:read';
+	public const SCOPE_USER_EDIT                  = 'user:edit';
+	public const SCOPE_USER_EDIT_FOLLOWS          = 'user:edit:follows';
+	public const SCOPE_USER_READ_BLOCKED_USERS    = 'user:read:blocked_users';
+	public const SCOPE_USER_MANAGE_BLOCKED_USERS  = 'user:manage:blocked_users';
+	public const SCOPE_USER_READ_BROADCAST        = 'user:read:broadcast';
+	public const SCOPE_USER_READ_EMAIL            = 'user:read:email';
+	public const SCOPE_USER_READ_SUBSCRIPTIONS    = 'user:read:subscriptions';
 
 	protected string $authURL          = 'https://id.twitch.tv/oauth2/authorize';
 	protected string $accessTokenURL   = 'https://id.twitch.tv/oauth2/token';
-	protected ?string $apiURL          = 'https://api.twitch.tv/helix';
+	protected ?string $apiURL          = 'https://api.twitch.tv';
 	protected ?string $userRevokeURL   = 'https://www.twitch.tv/settings/connections';
 	protected ?string $revokeURL       = 'https://id.twitch.tv/oauth2/revoke';
 	protected ?string $endpointMap     = TwitchEndpoints::class;
 	protected ?string $apiDocs         = 'https://dev.twitch.tv/docs/api/reference/';
 	protected ?string $applicationURL  = 'https://dev.twitch.tv/console/apps/create';
-#	protected string $authMethodHeader = 'OAuth';  // -> https://api.twitch.tv/kraken
-	protected string $authMethodHeader = 'Bearer'; // -> https://api.twitch.tv/helix
 	protected array $authHeaders       = ['Accept' => 'application/vnd.twitchtv.v5+json'];
 	protected array $apiHeaders        = ['Accept' => 'application/vnd.twitchtv.v5+json'];
 
 	protected array $defaultScopes     =  [
-		self::SCOPE_USER_READ,
+		self::SCOPE_USER_READ_EMAIL,
 	];
 
 	/**
